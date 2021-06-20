54a66b9830d4b9a1e7183f548cce9ab88c3f289b
melakutes <melakutessema04@gmail.com>
2021-06-20 13:42:00 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int k;
+for (k = '0'; k <= '9'; k++)
+{
+putchar(k);
+}
+for (k = 'a'; k <= 'f'; k++)
+{
+putchar(k);
+}
+putchar('\n');
+return (0);
+}
